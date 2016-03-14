6c68e4ab78b460f1921c0f711214758f58ce2832
pingzing <neil.mcalister@futurice.com>
2016-03-14 13:06:14 +0100

    Rework project structure, fix serious build weirdness

--- UwpReusables.TestBed/Properties/AssemblyInfo.cs
+++ UwpReusables.TestBed/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TestBed")]
+[assembly: AssemblyTitle("UwpReusables.TestBed")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TestBed")]
+[assembly: AssemblyProduct("UwpReusables.TestBed")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
